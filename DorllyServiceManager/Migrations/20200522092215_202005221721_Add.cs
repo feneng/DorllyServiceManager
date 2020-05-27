354486f34225698ac93ece3c6bbc73b558acc83a
feneng <354658353@qq.com>
2020-05-27 18:34:30 +0800

    完善功能

---
+++ DorllyServiceManager/Migrations/20200522092215_202005221721_Add.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DorllyServiceManager.Migrations
+{
+    public partial class _202005221721_Add : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
